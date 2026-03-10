021800b270f02d1f6e9583950d687b6fe6980fce
שיר מנדל <213917255@idf.il>
2026-03-10 12:49:27 +0200

    added routes

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,11 +1,14 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <>
-      <LoginPage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
