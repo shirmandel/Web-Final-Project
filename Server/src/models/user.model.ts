d70fd02582a83b3887817f406718c734d83ab423
שיר מנדל <213917255@idf.il>
2026-03-12 13:03:57 +0200

    added login register ang logout  routes

---
+++ Server/src/models/user.model.ts
@@ -0,0 +1,47 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IUser extends Document {
+  email: string;
+  username: string;
+  password?: string;
+  profileImage: string;
+  googleId?: string;
+  refreshTokens: string[];
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+    },
+    profileImage: {
+      type: String,
+      default: "",
+    },
+    // googleId: {
+    //   type: String,
+    //   sparse: true,
+    // },
+    refreshTokens: {
+      type: [String],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export default mongoose.model<IUser>("User", userSchema);
